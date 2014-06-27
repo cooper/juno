0b5d3e056bda4ccf967cc17b90e5acd057b7a94b
Matthew Barksdale <mattwb65@gmail.com>
2014-06-27 13:22:59 -0400

    Fixed more stupid typos

--- modules/SASL.module/SASL.pm
+++ modules/SASL.module/SASL.pm
@@ -20,7 +20,7 @@ use strict;
 use 5.010;
 
 use MIME::Base64;
-use M::Account qw(verify_password login_account);
+use M::Account qw(verify_account login_account);
 
 our ($api, $mod, $pool);
 
@@ -37,35 +37,27 @@ sub init {
 # Registration command: AUTHENTICATE
 sub rcmd_authenticate {
     my ($connection, $event, $arg) = @_;
-    say "got arg: $arg";
     $connection->early_reply(906, ':SASL authentication aborted') and return if $arg eq '*';
     $connection->early_reply(907, ':You have already authenticated using SASL') and return if $connection->{sasl};
     $connection->early_reply(908, 'PLAIN :are availiable SASL mechanisms') and return if uc $arg eq 'M';
     if (!exists $connection->{sasl_pending})
     {
         if (uc $arg ne 'PLAIN') {
-            say "not plain, $arg";
             $connection->early_reply(904, ':SASL authentication failed');
             return;
          } else {
-             say "asking for more data";
              $connection->send('AUTHENTICATE +');
              $connection->{sasl_pending} = 1;
          }
     } else {
-        say "falling into else";
         my (undef, $user, $password) = split('\0', decode_base64($arg));
-        say "user: $user password: $password";
-        my $act = verify_password($user, $password);
-        if ($act) {
-            say "accepted";
+        if (my $act = verify_account($user, $password)) {
             $connection->{sasl_account} = $act;
             $connection->{sasl} = 1;
-            $connection->early_reply(900, "$$connection{nick}!$$connection{user}\@$$connection{host} $$act{name} :You are now logged in as $$act{name}.");
+            $connection->early_reply(900, "$$connection{nick}!$$connection{ident}\@$$connection{host} $$act{name} :You are now logged in as $$act{name}.");
             $connection->early_reply(903, ':SASL authentication successful');
             delete $connection->{sasl_pending};
         } else {
-          say "failed";
           $connection->early_reply(904, ':SASL authentication failed');
           return;
         }
@@ -75,7 +67,9 @@ sub rcmd_authenticate {
 # user.new event
 sub user_new {
     my ($event, $user) = @_;
-    if ($user->{sasl_acount}) {
+    say "user_new called";
+    if ($user->{sasl_account}) {
+        say "sasl account found";
         login_account(delete $user->{sasl_account}, $user, undef, undef, 1);
     }
 }
