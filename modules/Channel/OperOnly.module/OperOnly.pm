68b5fd7eabac25a4112aa87f65cbfd7813ea3ff3
Mitchell Cooper <githubspam@rlygd.net>
2015-02-15 21:28:45 -0500

    9.86: fix broken Channel::OperOnly.

--- modules/Channel/OperOnly.module/OperOnly.pm
+++ modules/Channel/OperOnly.module/OperOnly.pm
@@ -22,36 +22,38 @@ use 5.010;
 our ($api, $mod, $pool);
 
 sub init {
+    
     # register oper only mode block.
     $mod->register_channel_mode_block(
         name => 'oper_only',
         code => \&cmode_operonly
     ) or return;
+    
     # register ERR_OPERONLY
     $mod->register_user_numeric(
         name   => 'ERR_OPERONLY',
         number => 520,
         format => '%s :Channel is IRC Operator only'
     ) or return;
+    
     # Hook on the can_join event to prevent joining a channel that is oper only
     $pool->on('user.can_join' => \&on_user_can_join, with_eo => 1, name => 'is.oper.only');
+    
     return 1;
 }
 
+# only opers can set
 sub cmode_operonly {
     my ($channel, $mode) = @_;
     $mode->{has_basic_status} or return;
-
-    my $source = $mode->{source};
-    return 1 if (!$source->isa('user') || $mode->{force});
-    return if !$source->has_mode('ircop');
-
-    return 1;
+    return 1 if !$mode->{source}->isa('user');
+    return $mode->{source}->is_mode('ircop');
 }
 
 sub on_user_can_join {
     my ($user, $event, $channel) = @_;
     return unless $channel->is_mode('oper_only');
+    return if $user->is_mode('ircop');
     $user->numeric(ERR_OPERONLY => $channel->name);
     $event->stop('channel_operonly');
 }
