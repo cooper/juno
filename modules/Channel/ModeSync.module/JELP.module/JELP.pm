9007a4e231d5d7978064927aa6e49996fbe0f981
Mitchell Cooper <githubspam@rlygd.net>
2016-09-23 12:05:47 -0400

    11.07: started work on Channel::ModeSync. #63.             unset modes locally when they are no longer available.

---
+++ modules/Channel/ModeSync.module/JELP.module/JELP.pm
@@ -0,0 +1,20 @@
+# Copyright (c) 2016, Mitchell Cooper
+#
+# @name:            'Channel::ModeSync::JELP'
+# @package:         'M::Channel::ModeSync::JELP'
+# @description:     'JELP mode synchronization'
+#
+# @author.name:     'Mitchell Cooper'
+# @author.website:  'https://github.com/cooper'
+#
+package M::Channel::ModeSync::JELP;
+
+use warnings;
+use strict;
+use 5.010;
+
+our ($api, $mod, $pool);
+
+
+
+$mod
--- modules/Channel/ModeSync.module/ModeSync.pm
+++ modules/Channel/ModeSync.module/ModeSync.pm
@@ -17,10 +17,35 @@ use warnings;
 use strict;
 use 5.010;
 
-our ($api, $mod, $pool);
+our ($api, $mod, $pool, $me);
 
 sub init {
+    $mod->add_companion_submodule('JELP::Base', 'JELP');
+    $pool->on(cmodes_changed => \&cmodes_changed, 'modesync');
+}
+
+sub cmodes_changed {
+    my (undef, $fire, $added, $removed) = @_;
+
+    # first, locally unset the modes which were removed
+    for my $channel ($pool->channels) {
+        my @all_modes;
+        for my $mode_type (@$removed) {
+
+            # get all modes of this type
+            my $modes = $channel->get_modes($mode_type);
+            next if !@$modes;
+
+            # invert the state
+            for (0..$#$modes) {
+                next if $_ % 2;
+                $modes->[$_] = '-'.$modes->[$_];
+            }
+
+            push @all_modes, @$modes;
+        }
+        $channel->do_modes_local($me, \@all_modes, 1, undef, 1);
+    }
 }
 
 $mod
-
